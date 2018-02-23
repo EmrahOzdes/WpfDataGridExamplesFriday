51f34291e4deed3b02d860f23992354b47eed29e
AREL <AREL@B107-04>
2018-02-23 14:47:52 +0300

    DataGrid added.

--- WpfDataGridExamples/MainWindow.xaml.cs
+++ WpfDataGridExamples/MainWindow.xaml.cs
@@ -13,16 +13,59 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfDataGridExamples
+namespace WpfSample
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Customers> list;
         public MainWindow()
         {
             InitializeComponent();
+            list = new List<Customers>()
+            {
+                new Customers() {AD="Ali",SOYAD="CAN" },
+                new Customers() {AD="Alihan",SOYAD="CANLI" },
+                new Customers() {AD="Alican",SOYAD="CANSIZ" },
+                new Customers() {AD="Aliye",SOYAD="CANMI" },
+                new Customers() {AD="Mahmut",SOYAD="CANNIMIZ" },
+                new Customers() {AD="Kemal",SOYAD="CANCAN" },
+                new Customers() {AD="Kemaliye",SOYAD="CANKAN" },
+                new Customers() {AD="Kelamin",SOYAD="CANISI" },
+
+            };
+            Customers lst = new Customers();
+            lst.AD = "Mehmet";
+            lst.SOYAD = "YAĞIZ";
+            list.Add(lst);
         }
+
+        private void btntikla_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Butona Tıklandı.");
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            MessageBox.Show(cmbBoxListe.SelectedItem.ToString());
+
+            MessageBox.Show(((ComboBoxItem)(cmbBoxListe.SelectedItem)).Content.ToString());
+        }
+        internal class Customers
+        {
+            public string AD { get; internal set; }
+            public string SOYAD { get; internal set; }
+        }
+        public void Window_Load(object sender,RoutedEventArgs e)
+        {
+            tb1.ItemsSource = list;
+        }
+
+
     }
+   
+
+
 }
